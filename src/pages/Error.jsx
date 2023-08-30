42dded8292e64b0deba2d89beee9708f9079cea0
Andarov <bekturandarob678@gmail.com>
2023-08-30 09:42:45 +0500

    Add info and styles to Error page and full ended

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Link, useRouteError } from 'react-router-dom'
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import errorImg from "../assets/img/error.png";
 
 const Error = () => {
-    const error = useRouteError()
+  const error = useRouteError();
   return (
-    <div className='w-full max-w-[1320px] px-5 mx-auto py-20'>
-        <h1>Error</h1>
-        <p>{error.message}</p>
-        <Link to='/'>Ortga qaytish</Link>
+    <div className="w-full max-w-[1320px] px-5 mx-auto py-20">
+      <div className="flex flex-col items-center w-full max-w-md mx-auto">
+        <img className="w-full h-52 mb-8" src={errorImg} alt="Error image" />
+        <h1 className="text-3xl font-bold mb-3">Error</h1>
+        <p className="text-lg mb-2">{error.message}</p>
+        <Link className="bg-green-400 px-5 py-1 rounded-md" to="/">Ortga qaytish</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
